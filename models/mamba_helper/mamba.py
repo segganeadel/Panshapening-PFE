10d951b73546f5bc6d61045e89904088821e3e56
Adel Seggane <segganeadel@gmail.com>
2024-06-04 15:05:37 +0100

    refactor: Remove commented code and unused variables in mamba.py

--- models/mamba_helper/mamba.py
+++ models/mamba_helper/mamba.py
@@ -17,13 +17,13 @@ class PatchEmbed(nn.Module):
                  embed_dim=96, 
                  norm_layer=None):
         super().__init__()
-        # img_size = to_2tuple(img_size)
-        # patch_size = to_2tuple(patch_size)
-        # patches_resolution = [img_size[0] // patch_size[0], img_size[1] // patch_size[1]]
-        # self.img_size = img_size
-        # self.patch_size = patch_size
-        # self.patches_resolution = patches_resolution
-        # self.num_patches = patches_resolution[0] * patches_resolution[1]
+        img_size = to_2tuple(img_size)
+        patch_size = to_2tuple(patch_size)
+        patches_resolution = [img_size[0] // patch_size[0], img_size[1] // patch_size[1]]
+        self.img_size = img_size
+        self.patch_size = patch_size
+        self.patches_resolution = patches_resolution
+        self.num_patches = patches_resolution[0] * patches_resolution[1]
 
         self.in_chans = in_chans
         self.embed_dim = embed_dim
@@ -379,15 +379,22 @@ class deepFuse(nn.Module):
                  spectral_num=4,
                  embed_dim=96,
                  depths=(2, 2),
+                 drop_rate=0.,
                  d_state = 16,
                  mlp_ratio=2.,
                  drop_path_rate=0.1,
                  norm_layer=nn.LayerNorm,
                  patch_norm=True,
+                 upscale=2,
+                 img_range=1.,
                  **kwargs):
         super(deepFuse, self).__init__()
         num_in_ch = spectral_num
         num_out_ch = spectral_num
+        num_feat = 64
+        self.img_range = img_range
+        self.mean = torch.zeros(1, 1, 1, 1)
+        self.upscale = upscale
         self.mlp_ratio=mlp_ratio
         # ------------------------- 1, shallow feature extraction ------------------------- #
         self.conv_first = nn.Conv2d(num_in_ch, embed_dim, 3, 1, 1)
@@ -415,6 +422,8 @@ class deepFuse(nn.Module):
             in_chans=embed_dim,
             embed_dim=embed_dim,
             norm_layer=norm_layer if self.patch_norm else None)
+
+        self.pos_drop = nn.Dropout(p=drop_rate)
 
         # stochastic depth
         dpr = [x.item() for x in torch.linspace(0, drop_path_rate, sum(depths))]  # stochastic depth decay rule
@@ -459,6 +468,8 @@ class deepFuse(nn.Module):
         x_size = (x.shape[2], x.shape[3])
         x = self.patch_embed(x) # N,L,C
 
+        x = self.pos_drop(x)
+
         for layer in self.layers:
             x = layer(x, x_size)
 
@@ -469,6 +480,8 @@ class deepFuse(nn.Module):
         return x
 
     def forward(self, x):
+        self.mean = self.mean.type_as(x)
+        x = (x - self.mean) * self.img_range
 
         x_first = self.conv_first(x)
         res = self.conv_after_body(self.forward_features(x_first)) + x_first
