456245fc41e0122c20e3cc08f78daadcf1a291ab
Adel Seggane <segganeadel@gmail.com>
2024-06-04 18:52:52 +0100

    refactor: Remove unused code and comments in mamba.py

--- models/mamba_helper/mamba.py
+++ models/mamba_helper/mamba.py
@@ -403,7 +403,7 @@ class deepFuse(nn.Module):
         self.mean = torch.zeros(1, 1, 1, 1)
         self.mlp_ratio=mlp_ratio
         # ------------------------- 1, shallow feature extraction ------------------------- #
-        self.conv_first = nn.Conv2d(num_in_ch, embed_dim, 3, 1, 1)
+        # self.conv_first = nn.Conv2d(num_in_ch, embed_dim, 3, 1, 1)
 
         # ------------------------- 2, deep feature extraction ------------------------- #
         self.num_layers = len(depths)
@@ -486,10 +486,11 @@ class deepFuse(nn.Module):
         return x
 
     def forward(self, x):
-        self.mean = self.mean.type_as(x)
-        x = (x - self.mean)
+        # self.mean = self.mean.type_as(x)
+        # x = (x - self.mean)
 
-        x_first = self.conv_first(x)
+        # x_first = self.conv_first(x)
+        x_first = x
         
         res = self.conv_after_body(self.forward_features(x_first)) + x_first
         x = x + self.conv_last(res)
