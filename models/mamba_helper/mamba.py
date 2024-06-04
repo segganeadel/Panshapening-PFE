741353ed02ad54dff3a1cf0bae2ecc7435349c5d
Adel Seggane <segganeadel@gmail.com>
2024-06-04 16:49:47 +0100

    refactor: Update PatchEmbed in mamba.py to use a variable patch size

--- models/mamba_helper/mamba.py
+++ models/mamba_helper/mamba.py
@@ -9,6 +9,18 @@ from timm.models.layers import DropPath, to_2tuple, trunc_normal_
 from mamba_ssm.ops.selective_scan_interface import selective_scan_fn
 from einops import rearrange, repeat
 
+class OverlapPatchEmbed(nn.Module):
+    def __init__(self, in_c=3, embed_dim=96, bias=False):
+        super(OverlapPatchEmbed, self).__init__()
+
+        self.proj = nn.Conv2d(in_c, embed_dim, kernel_size=3, stride=1, padding=1, bias=bias)
+
+    def forward(self, x):
+        x = self.proj(x)
+        x = rearrange(x, "b c h w -> b (h w) c").contiguous()
+        return x
+
+
 class PatchEmbed(nn.Module):
     def __init__(self, 
                  img_size=224, 
@@ -28,7 +40,7 @@ class PatchEmbed(nn.Module):
         self.in_chans = in_chans
         self.embed_dim = embed_dim
 
-        self.proj = nn.Conv2d(in_chans, embed_dim, kernel_size= patch_size, stride=1, padding=1, bias=False)
+        # self.proj = nn.Conv2d(in_chans, embed_dim, kernel_size= patch_size, stride=1, padding=1, bias=False)
 
         if norm_layer is not None:
             self.norm = norm_layer(embed_dim)
@@ -38,8 +50,8 @@ class PatchEmbed(nn.Module):
     def forward(self, x):
         if self.norm is not None:
             x = self.norm(x)
-        x = self.proj(x)
-        x = rearrange(x, "b c h w -> b (h w) c").contiguous()
+        # x = self.proj(x)
+        # x = rearrange(x, "b c h w -> b (h w) c").contiguous()
         return x
 
 class PatchUnEmbed(nn.Module):
@@ -382,22 +394,18 @@ class deepFuse(nn.Module):
                  spectral_num=4,
                  embed_dim=96,
                  depths=(2, 2),
-                 drop_rate=0.,
                  d_state = 16,
                  mlp_ratio=2.,
                  drop_path_rate=0.1,
                  norm_layer=nn.LayerNorm,
                  patch_norm=True,
-                 upscale=2,
                  img_range=1.,
                  **kwargs):
         super(deepFuse, self).__init__()
         num_in_ch = spectral_num
         num_out_ch = spectral_num
-        num_feat = 64
         self.img_range = img_range
         self.mean = torch.zeros(1, 1, 1, 1)
-        self.upscale = upscale
         self.mlp_ratio=mlp_ratio
         # ------------------------- 1, shallow feature extraction ------------------------- #
         self.conv_first = nn.Conv2d(num_in_ch, embed_dim, 3, 1, 1)
@@ -407,6 +415,9 @@ class deepFuse(nn.Module):
         self.embed_dim = embed_dim
         self.patch_norm = patch_norm
         self.num_features = embed_dim
+        # overlap patch embedding
+        
+        self.overlap_patch_embed = OverlapPatchEmbed(in_c=num_in_ch, embed_dim=embed_dim)
 
         # transfer 2D feature map into 1D token sequence, pay attention to whether using normalization
         self.patch_embed = PatchEmbed(
@@ -425,8 +436,6 @@ class deepFuse(nn.Module):
             in_chans=embed_dim,
             embed_dim=embed_dim,
             norm_layer=norm_layer if self.patch_norm else None)
-
-        self.pos_drop = nn.Dropout(p=drop_rate)
 
         # stochastic depth
         dpr = [x.item() for x in torch.linspace(0, drop_path_rate, sum(depths))]  # stochastic depth decay rule
@@ -469,9 +478,7 @@ class deepFuse(nn.Module):
 
     def forward_features(self, x):
         x_size = (x.shape[2], x.shape[3])
-        x = self.patch_embed(x) # N,L,C
-
-        x = self.pos_drop(x)
+        x = self.overlap_patch_embed(x) # N,L,C
 
         for layer in self.layers:
             x = layer(x, x_size)
