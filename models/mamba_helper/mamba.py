637d3aaeb97b4e2e9dedd9c313eb053567db2ff5
Adel Seggane <segganeadel@gmail.com>
2024-06-04 17:10:56 +0100

    refactor: Remove OverlapPatchEmbed class and update PatchEmbed in mamba.py

--- models/mamba_helper/mamba.py
+++ models/mamba_helper/mamba.py
@@ -9,6 +9,16 @@ from timm.models.layers import DropPath, to_2tuple, trunc_normal_
 from mamba_ssm.ops.selective_scan_interface import selective_scan_fn
 from einops import rearrange, repeat
 
+class OverlapPatchEmbed(nn.Module):
+    def __init__(self, in_c=3, embed_dim=96, bias=False):
+        super(OverlapPatchEmbed, self).__init__()
+
+        self.proj = nn.Conv2d(in_c, embed_dim, kernel_size=3, stride=1, padding=1, bias=bias)
+
+    def forward(self, x):
+        x = self.proj(x)
+        x = rearrange(x, "b c h w -> b (h w) c").contiguous()
+        return x
 
 class PatchEmbed(nn.Module):
     def __init__(self, 
@@ -410,6 +420,10 @@ class deepFuse(nn.Module):
         self.patch_norm = patch_norm
         self.num_features = embed_dim
 
+        # overlap patch embedding
+        self.overlap_patch_embed = OverlapPatchEmbed(in_c=num_in_ch, embed_dim=embed_dim)
+
+
         # transfer 2D feature map into 1D token sequence, pay attention to whether using normalization
         self.patch_embed = PatchEmbed(
             img_size=img_size,
@@ -471,6 +485,7 @@ class deepFuse(nn.Module):
 
     def forward_features(self, x):
         x_size = (x.shape[2], x.shape[3])
+    
         x = self.patch_embed(x) # N,L,C
 
         x = self.pos_drop(x)
@@ -489,6 +504,7 @@ class deepFuse(nn.Module):
         x = (x - self.mean) * self.img_range
 
         x_first = self.conv_first(x)
+        
         res = self.conv_after_body(self.forward_features(x_first)) + x_first
         x = x + self.conv_last(res)
 
