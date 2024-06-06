bc48f100e96f32634c1d99bbecb473ac52d593d7
Belachar lamia <99986124+Lamia2428@users.noreply.github.com>
2024-06-06 15:55:55 +0200

    ll

--- models/mamba_helper/mamba.py
+++ models/mamba_helper/mamba.py
@@ -560,13 +560,7 @@ class ShallowFeatureExtractor(nn.Module):
         x = self.bn1(x)
         x = self.relu1(x)
 
-        x = self.conv2(x)
-        x = self.bn2(x)
-        x = self.relu2(x)
-
-        x = self.conv3(x)
-        x = self.bn3(x)
-        x = self.relu3(x)
+        
 
         x = self.dilated_conv(x)
 
