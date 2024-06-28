4ba401a45e9a10e11758eb8fafcd6949f30fb29d
Adel Seggane <segganeadel@gmail.com>
2024-06-29 00:22:47 +0100

    added other datamodule

--- datamodule_mat_other.py
+++ datamodule_mat_other.py
@@ -9,7 +9,9 @@ except:
 
 class PANDataModule(L.LightningDataModule):
     def __init__(self,
-                 data_dir, 
+                 data_dir,
+                 satelite,
+                 channels, 
                  img_scale, 
                  highpass, 
                  num_workers = 0 , 
@@ -18,6 +20,8 @@ class PANDataModule(L.LightningDataModule):
         super().__init__()
 
         self.data_dir = data_dir
+        self.satelite = satelite
+        self.channels = channels
         self.img_scale = img_scale
         self.highpass = highpass
         self.num_workers = num_workers
@@ -31,23 +35,23 @@ class PANDataModule(L.LightningDataModule):
         train_file_path = os.path.join(self.data_dir, 'train')
 
         # creating the dataset
-        data = Dataset_mat_rr(train_file_path, img_scale=self.img_scale, highpass=self.highpass)
+        data = Dataset_mat_rr(train_file_path, sattelite=self.satelite, channels=self.channels, img_scale=self.img_scale, highpass=self.highpass)
         return DataLoader(data, batch_size=self.batch_size, num_workers=self.num_workers, persistent_workers=True, shuffle=self.shuffle_train, pin_memory=True)
 
     def val_dataloader(self):
         val_file_path = os.path.join(self.data_dir, 'valid')
 
-        data = Dataset_mat_rr(val_file_path, img_scale=self.img_scale, highpass=self.highpass)
+        data = Dataset_mat_rr(val_file_path, sattelite=self.satelite, channels=self.channels, img_scale=self.img_scale, highpass=self.highpass)
         return DataLoader(data, batch_size=self.batch_size, num_workers=self.num_workers, persistent_workers=True)
 
     def test_dataloader(self):
         test_file_path = os.path.join(self.data_dir, 'test')
 
-        data = Dataset_mat_rr(test_file_path, img_scale=self.img_scale, highpass=self.highpass)
+        data = Dataset_mat_rr(test_file_path, sattelite=self.satelite, channels=self.channels, img_scale=self.img_scale, highpass=self.highpass)
         return DataLoader(data, batch_size=self.batch_size, num_workers=self.num_workers)    
     
     def predict_dataloader(self):
         predict_file_path = os.path.join(self.data_dir, 'predict')
 
-        data = Dataset_mat_fr(predict_file_path, img_scale=self.img_scale, highpass=self.highpass)
+        data = Dataset_mat_rr(predict_file_path, sattelite=self.satelite, channels=self.channels, img_scale=self.img_scale, highpass=self.highpass)
         return DataLoader(data, batch_size=self.batch_size, num_workers=self.num_workers)
